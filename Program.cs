e77d06f48ff9fcefa388893fe0bf577cea271080
Кирилл Змановский <kirya_zm@mail.ru>
2022-12-09 19:46:27 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorio
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        }
+    }
+    abstract class Item
+    { }
+
+    class Battery : Item
+    {
+        
+    }
+
+    class Medkit : Item
+    { }
+
+    abstract class Replicator
+    {
+        public abstract Item FactoryMethod();
+    }
+
+    class ConcreteCreatorA : Replicator
+    {
+        public override Item FactoryMethod() { return new Battery(); }
+    }
+
+    class ConcreteCreatorB : Replicator
+    {
+        public override Item FactoryMethod() { return new Medkit(); }
+    }
+}
